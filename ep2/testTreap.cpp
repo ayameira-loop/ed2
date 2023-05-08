b4ce352f59fc582f3870b49c46fa5078ab440930
ayameira <72822950+ayameira-loop@users.noreply.github.com>
2023-05-08 08:59:11 -0300

    treaps - falta testar

---
+++ ep2/testTreap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "Treap.h"
+
+int main() {
+    Treap<int, std::string> arvore;
+    srand(1224);
+    
+    arvore.add(1, "1");
+    arvore.add(4, "4");
+    arvore.add(2, "2");
+    arvore.add(0, "0");
+    arvore.add(5, "5");
+    //arvore.add(3, "3");
+    //std::cout << arvore.value(3);
+
+    arvore.print();
+    return 0;
+}
