07be170cf875e311efd106ff0b52833e018131f7
ayameira <72822950+ayameira-loop@users.noreply.github.com>
2023-05-20 16:43:12 -0300

    main looks good

--- ep2/main.cpp
+++ ep2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include "VetorDinamicoOrdenado.h"
 #include "Palavra.h"
@@ -31,134 +32,145 @@ void removeDuplicates(std::string* arr, int size) {
 }
 
 template<typename E>
-void doStuff(E estrutura, std::string words[], int N) {
+void doStuff(E estrutura, std::string words[], int N, std::vector<std::string> commands) {
+    // populate structure
     std::string cleanWords[N];
-
     for (size_t i = 0; i < N; i++) {
         std::string cleanWord = removeCharacters(words[i]);
         cleanWords[i] = cleanWord;
         Palavra p = Palavra(cleanWord);
         estrutura.add(cleanWord, p);
     }
-    estrutura.print();
 
-    // F: palavras mais frequentes
-    int highestF = 1;
-    int tamHighest = 0; // numero de palavras q ocorreram highestF vezes
-    std::string highest[N];
+    // follow commands
+    for (const auto& command : commands) {
+        if (command == "F") {
+            // F: palavras mais frequentes
+            int highestF = 1;
+            int tamHighest = 0; // numero de palavras q ocorreram highestF vezes
+            std::string highest[N];
 
-    for (int i = 0; i < N; i++) {
-        std::string word = cleanWords[i];
-        Palavra p = estrutura.value(word);
-        if (p.getOcorrencias() > highestF) {
-            highestF = p.getOcorrencias();
-            highest[0] = word;
-            tamHighest = 1;
-        } else if (p.getOcorrencias() == highestF) {
-            highest[tamHighest] = word;
-            tamHighest++;
-        }
-    }
-    removeDuplicates(highest, tamHighest);
-    std::cout << "Palavra(s) mais frequente(s) (frequencia = " << highestF << "): " << std::endl;
-    for (int i = 0; i < tamHighest; i++) {
-        if (highest[i] != "") {
-            std::cout << highest[i] << " ";
-        }
-    }
-    std::cout << std::endl;
-
-    // O: dada uma palavra, quantas vezes ocorre no texto
-
-    // L: quais as palavras mais longas
-    int length = 1;
-    int tamLongest = 0; // numero de palavras q tem length tamanho
-    std::string longest_words[N];
-
-    for (int i = 0; i < N; i++) {
-        std::string word = cleanWords[i];
-        Palavra p = estrutura.value(word);
-        if (p.getNumLetras() > length) {
-            length = p.getNumLetras();
-            longest_words[0] = word;
-            tamLongest = 1;
-        } else if (p.getNumLetras() == length) {
-            longest_words[tamLongest] = word;
-            tamLongest++;
-        }
-    }
-    removeDuplicates(longest_words, tamLongest);
-    std::cout << "Palavra(s) mais longa(s) (# letras = " << length << "): " << std::endl;
-    for (int i = 0; i < tamLongest; i++) {
-        if (longest_words[i] != "") {
-            std::cout << longest_words[i] << " ";
-        }
-    }
-    std::cout << std::endl;
-
-    // SR: quais as maiores palavras que nao repetem letras
-    int lengthSR = 1;
-    int tamLongestSR = 0; // numero de palavras q tem lengthSR tamanho
-    std::string longest_words_SR[N];
-
-    for (int i = 0; i < N; i++) {
-        std::string word = cleanWords[i];
-        Palavra p = estrutura.value(word);
-        if (p.getTemRepetida()) continue;
-        if (p.getNumLetras() > lengthSR) {
-            lengthSR = p.getNumLetras();
-            longest_words_SR[0] = word;
-            tamLongestSR = 1;
-        } else if (p.getNumLetras() == lengthSR) {
-            longest_words_SR[tamLongestSR] = word;
-            tamLongestSR++;
-        }
-    }
-    removeDuplicates(longest_words_SR, tamLongestSR);
-    std::cout << "Palavra(s) mais longa(s) sem letras repetidas (# letras = " << lengthSR << "): " << std::endl;
-    for (int i = 0; i < tamLongestSR; i++) {
-        if (longest_words_SR[i] != "") {
-            std::cout << longest_words_SR[i] << " ";
-        }
-    }
-    std::cout << std::endl;
-
-    // VD: quais as menores palavras com mais vogais sem repeticao
-    int mostVowels = 1;
-    int tamMost = 0; // numero de palavras q tem lengthSR tamanho
-    std::string wordsWithMostVowels[N];
-
-    for (int i = 0; i < N; i++) {
-        std::string word = cleanWords[i];
-        Palavra p = estrutura.value(word);
-        if (p.getNumVogaisSemRepeticao() > mostVowels) {
-            mostVowels = p.getNumVogaisSemRepeticao();
-            wordsWithMostVowels[0] = word;
-            tamMost = 1;
-        } else if (p.getNumVogaisSemRepeticao() == mostVowels) {
-            wordsWithMostVowels[tamMost] = word;
-            tamMost++;
-        }
-    }
-    removeDuplicates(wordsWithMostVowels, tamMost);
-    int shortestLengthVD = 5000;
-    // encontra menor palavra em wordsWithMostVowels
-    for (int i = 0; i < tamMost; i++) {
-        if (wordsWithMostVowels[i] != "") {
-            int len = wordsWithMostVowels[i].length();
-            if (len < shortestLengthVD) {
-                shortestLengthVD = len;
+            for (int i = 0; i < N; i++) {
+                std::string word = cleanWords[i];
+                Palavra p = estrutura.value(word);
+                if (p.getOcorrencias() > highestF) {
+                    highestF = p.getOcorrencias();
+                    highest[0] = word;
+                    tamHighest = 1;
+                } else if (p.getOcorrencias() == highestF) {
+                    highest[tamHighest] = word;
+                    tamHighest++;
+                }
             }
+            removeDuplicates(highest, tamHighest);
+            std::cout << "Palavra(s) mais frequente(s) (frequencia = " << highestF << "): " << std::endl;
+            for (int i = 0; i < tamHighest; i++) {
+                if (highest[i] != "") {
+                    std::cout << highest[i] << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+        if (command[0] == 'O') {
+            std::istringstream iss(command);
+            std::string trash, word2search;
+            iss >> trash >> word2search;
+            std::cout << estrutura.value(word2search).getOcorrencias() << std::endl;
+        }
+        if (command == "L") {
+            // L: quais as palavras mais longas
+            int length = 1;
+            int tamLongest = 0; // numero de palavras q tem length tamanho
+            std::string longest_words[N];
+
+            for (int i = 0; i < N; i++) {
+                std::string word = cleanWords[i];
+                Palavra p = estrutura.value(word);
+                if (p.getNumLetras() > length) {
+                    length = p.getNumLetras();
+                    longest_words[0] = word;
+                    tamLongest = 1;
+                } else if (p.getNumLetras() == length) {
+                    longest_words[tamLongest] = word;
+                    tamLongest++;
+                }
+            }
+            removeDuplicates(longest_words, tamLongest);
+            std::cout << "Palavra(s) mais longa(s) (# letras = " << length << "): " << std::endl;
+            for (int i = 0; i < tamLongest; i++) {
+                if (longest_words[i] != "") {
+                    std::cout << longest_words[i] << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+        if (command == "SR") {
+            // SR: quais as maiores palavras que nao repetem letras
+            int lengthSR = 1;
+            int tamLongestSR = 0; // numero de palavras q tem lengthSR tamanho
+            std::string longest_words_SR[N];
+
+            for (int i = 0; i < N; i++) {
+                std::string word = cleanWords[i];
+                Palavra p = estrutura.value(word);
+                if (p.getTemRepetida()) continue;
+                if (p.getNumLetras() > lengthSR) {
+                    lengthSR = p.getNumLetras();
+                    longest_words_SR[0] = word;
+                    tamLongestSR = 1;
+                } else if (p.getNumLetras() == lengthSR) {
+                    longest_words_SR[tamLongestSR] = word;
+                    tamLongestSR++;
+                }
+            }
+            removeDuplicates(longest_words_SR, tamLongestSR);
+            std::cout << "Palavra(s) mais longa(s) sem letras repetidas (# letras = " << lengthSR << "): " << std::endl;
+            for (int i = 0; i < tamLongestSR; i++) {
+                if (longest_words_SR[i] != "") {
+                    std::cout << longest_words_SR[i] << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+        if (command == "VD") {
+            // VD: quais as menores palavras com mais vogais sem repeticao
+            int mostVowels = 1;
+            int tamMost = 0; // numero de palavras q tem lengthSR tamanho
+            std::string wordsWithMostVowels[N];
+
+            for (int i = 0; i < N; i++) {
+                std::string word = cleanWords[i];
+                Palavra p = estrutura.value(word);
+                if (p.getNumVogaisSemRepeticao() > mostVowels) {
+                    mostVowels = p.getNumVogaisSemRepeticao();
+                    wordsWithMostVowels[0] = word;
+                    tamMost = 1;
+                } else if (p.getNumVogaisSemRepeticao() == mostVowels) {
+                    wordsWithMostVowels[tamMost] = word;
+                    tamMost++;
+                }
+            }
+            removeDuplicates(wordsWithMostVowels, tamMost);
+            int shortestLengthVD = 5000;
+            // encontra menor palavra em wordsWithMostVowels
+            for (int i = 0; i < tamMost; i++) {
+                if (wordsWithMostVowels[i] != "") {
+                    int len = wordsWithMostVowels[i].length();
+                    if (len < shortestLengthVD) {
+                        shortestLengthVD = len;
+                    }
+                }
+            }
+            std::cout << "Palavra(s) com mais vogais sem repeticao (# vogais = " << mostVowels << ") e menor tamanho (# letras = " << shortestLengthVD << "): " << std::endl;
+            // printa todas as palavras com esse tamanho
+            for (int i = 0; i < tamMost; i++) {
+                if (wordsWithMostVowels[i].length() == shortestLengthVD) {
+                    std::cout << wordsWithMostVowels[i] << " ";
+                }
+            }
+            std::cout << std::endl;
         }
     }
-    std::cout << "Palavra(s) com mais vogais sem repeticao (# vogais = " << mostVowels << ") e menor tamanho (# letras = " << shortestLengthVD << "): " << std::endl;
-    // printa todas as palavras com esse tamanho
-    for (int i = 0; i < tamMost; i++) {
-        if (wordsWithMostVowels[i].length() == shortestLengthVD) {
-            std::cout << wordsWithMostVowels[i] << " ";
-        }
-    }
-    std::cout << std::endl;
 }
 
 int main() {
@@ -166,36 +178,39 @@ int main() {
     std::string E;
     std::cin >> E;
 
+
+    // build array with N words
     int N; 
     std::cin >> N;
     std::string words[N];
-
     for (int i = 0; i < N; i++) {
         cin >> words[i];
     }
 
-    if (E == "VO") {
-        std::cout << "eh vo" << std::endl;
-        VetorDinamicoOrdenado<std::string, Palavra> estrutura;
-        doStuff(estrutura, words, N);
-    } else if (E == "ABB") {
-        ArvoreBuscaBinaria<std::string, Palavra> estrutura;
-        doStuff(estrutura, words, N);
-    } else if (E == "TR") {
-        Treap<std::string, Palavra> estrutura;
-        doStuff(estrutura, words, N);
-    } else if (E == "ARN") {
-        RBTree<std::string, Palavra> estrutura;
-        doStuff(estrutura, words, N);
-    } else {
-        std::cout << "Invalid data structure." << std::endl;
-        return -1;
+    // parse commands
+    std::string line;
+    std::vector<std::string> commands;
+    while (std::getline(std::cin, line)) {
+        commands.push_back(line);
     }
     
-
-
-
-
+    // choose structure 
+    if (E == "VO") {
+        VetorDinamicoOrdenado<std::string, Palavra> estrutura;
+        doStuff(estrutura, words, N, commands);
+    } else if (E == "ABB") {
+        ArvoreBuscaBinaria<std::string, Palavra> estrutura;
+        doStuff(estrutura, words, N, commands);
+    } else if (E == "TR") {
+        Treap<std::string, Palavra> estrutura;
+        doStuff(estrutura, words, N, commands);
+    } else if (E == "ARN") {
+        RBTree<std::string, Palavra> estrutura;
+        doStuff(estrutura, words, N, commands);
+    } else {
+        std::cout << "Invalid data structure" << std::endl;
+        return -1;
+    }
     
     return 0;
 }
