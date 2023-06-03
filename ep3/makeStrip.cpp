cd79f3664a3d0914911f0b86f8ca6741d04b5937
ayameira <72822950+ayameira-loop@users.noreply.github.com>
2023-06-02 21:01:50 -0300

    chunk strip using random steps backwards at each chunk

--- ep3/makeStrip.cpp
+++ ep3/makeStrip.cpp
@@ -19,18 +19,12 @@ std::string generateDNAString(int N) {
 }
 
 // Function to break DNA string into chunks
-void breakIntoChunks(const std::string& dnaString, const std::vector<int>& chunkSizes) {
-    int startIndex = 0;
-    int numChunks = chunkSizes.size();
-
-    for (int i = 0; i < numChunks; i++) {
-        int chunkSize = chunkSizes[i];
-        std::cout << "Chunk " << i + 1 << ": ";
-        for (int j = startIndex; j < startIndex + chunkSize && j < dnaString.length(); j++) {
-            std::cout << dnaString[j];
-        }
-        std::cout << std::endl;
-        startIndex += chunkSize;
+void breakIntoChunks(const std::string& word, const std::vector<int>& startOfChunks, const std::vector<int>& endOfChunks) {
+    for (int i = 0; i < startOfChunks.size(); i++) {
+        int start = startOfChunks[i];
+        int end = endOfChunks[i];
+        std::string chunk = word.substr(start, end - start + 1);
+        std::cout << "Chunk " << i + 1 << ": " << chunk << std::endl;
     }
 }
 
@@ -41,35 +35,42 @@ int main() {
     std::cout << "Enter the length of the DNA string: ";
     std::cin >> N;
 
-    int meanChunkSize; // Mean chunk size
-    std::cout << "Enter the mean chunk size: ";
+    int meanChunkSize; // Mean chunk length
+    std::cout << "Enter the mean chunk length: ";
     std::cin >> meanChunkSize;
 
     int var; // Variance
     std::cout << "Enter the variance: ";
     std::cin >> var;
 
-    std::vector<int> chunkSizes(0);
+    std::vector<int> startOfChunks(0);
+    std::vector<int> endOfChunks(0);
+
     std::cout << "Breaking DNA string into chunks:\n";
 
-    int remaining = N; // How many letters are left in the string (not chunked yet)
-    int count = 0;
-    while (remaining > 0) {
-        int signal = (rand() % 2) * 2 - 1;
-        int chunkSize = meanChunkSize + (rand() % var+1) * signal; // Generate random chunk size with variance of -var, 0, or var
-        //std::cout << chunkSize << std::endl;
-        if (remaining - chunkSize < 0) {
-            chunkSize = remaining;
-        }
-        chunkSizes.push_back(chunkSize);
-        remaining = remaining - chunkSize;
-    }
+    int start = 0;
+    int end = 0;
+    int step = 0;
 
+    while (start < N-1 && end < N-1) {
+        start = end - step; // Goes back about half of what it moved forward 
+        if (start < 0)  start = end + step;
+        step = (meanChunkSize + (rand() % var+1))/2;
+        if (start > N-1)    continue;
+        startOfChunks.push_back(start);
+        int signal = (rand() % 2) * 2 - 1;
+        int chunkSize = meanChunkSize + (rand() % var+1) * signal;
+        if (start + chunkSize > N-1)
+            chunkSize = (N-1) - start; 
+        end = start + chunkSize;
+        endOfChunks.push_back(end);
+    }
+    
     std::string dnaString = generateDNAString(N);
     std::cout << "Generated DNA string: " << dnaString << std::endl;
 
     std::cout << "\nBreaking DNA string into chunks:\n";
-    breakIntoChunks(dnaString, chunkSizes);
+    breakIntoChunks(dnaString, startOfChunks, endOfChunks);
 
     return 0;
 }
