692a6a0438a7221065a680bf0f211f6f6798b957
ayameira <72822950+ayameira-loop@users.noreply.github.com>
2023-07-11 09:06:04 -0300

    incluindo intervalo e conjunto

--- ep4/main.cpp
+++ ep4/main.cpp
@@ -4,44 +4,21 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-/*
-bool reconhece(std::string word, Graph G) {
-    std::vector<bool> marked(G.size(), false);
-    std::stack<int> reached;
-    std::string regex = G.getRegex();
-    G.dfs(0, marked);
-    for (int i=0; i < word.length(); i++) {
-        for (int j=0; j < marked.size()-1; j++) {
-            if (marked[j] && (regex[j] == word[i] || ((j-1 < 0 || regex[j-1] != '\\')&& regex[j] == '.')) ) {
-                std::cout << "Leio " << marked[j] << std::endl;
-                reached.push(j+1);
-            }
-        }
-        for (int k=0; k < marked.size(); k++) {
-            marked[k] = false;
-        }
-        while (!reached.empty()) {
-            G.dfs(reached.top(), marked);
-            reached.pop();
-        }
-    }
-    //marked[regex.length()] == Estado de aceitação
-    return marked[regex.length()];
-}
-*/
-bool reconhece(std::string word, Graph G) {
-    std::string regex = G.getRegex();
-    std::vector<bool> atingidos(G.size(), false);
+
+bool reconhece(string word, Graph G) {
+    string regex = G.getRegex();
+    vector<bool> atingidos(G.size(), false);
     G.dfs(0, atingidos);
     
     for (int i=0; i < word.length(); i++) {
-        std::vector<bool> prox(G.size(), false);
+        vector<bool> prox(G.size(), false);
         for (int j=0; j < atingidos.size()-1; j++) {
             if (atingidos[j] && (regex[j] == word[i] || regex[j] == '.')) {
-                prox[j+1] = true;//
+                prox[j+1] = true;
+                cout << "leio " << regex[j] << endl;
             }
         }
-        std::vector<bool> marc(G.size());
+        vector<bool> marc(G.size());
         for (int k=0; k < atingidos.size(); k++)    atingidos[k] = false;
         for (int j=0; j < G.size(); j++) {
             if (prox[j]) {
@@ -55,17 +32,74 @@ bool reconhece(std::string word, Graph G) {
     }
     return atingidos[G.size()-1];
 }
-int main(void) {
-    std::string re = "(A*CG|A*TA|AAG*T)*";
 
-    Graph G(re.length()+1, re);
+string treatRegex(string regex) {
+    stack<char> newString;
+    stack<int> brackets;
+
+    std::string output = regex;
+    
+    for (int i=0; i<regex.length(); i++) {
+        if (regex[i] == '[') {
+            brackets.push(i);
+        }
+        if (regex[i] == ']') {
+            brackets.push(i);
+        }
+    }
+    string substring = "";
+    while (brackets.size() > 1) {
+        int end = brackets.top();
+        brackets.pop();
+        int start = brackets.top();
+        brackets.pop();
+        substring = regex.substr(start + 1, end - start - 1);
+        // Checa se é um conjunto ou intervalo
+        string expandedSubset = "";
+        if (substring.length() > 2 && substring[1] == '-') {
+            // intervalo
+            char rangeStart = substring[0];
+            char rangeEnd = substring[2];
+            
+            // Expand the range subset
+            for (char c = rangeStart; c <= rangeEnd; ++c) {
+                expandedSubset += c;
+                if (c != rangeEnd) {
+                    expandedSubset += '|';
+                }
+            }
+        } else {
+            // conjunto
+            for (int i=0; i < substring.length(); i++) {
+                expandedSubset += substring[i];
+                if (i != substring.length()-1) {
+                    expandedSubset += '|';
+                }
+            }
+        }
+        //regex.replace(start, end - start + 1, "(" + expandedSubset + ")");
+        regex.replace(start, end - start + 1, expandedSubset);
+        cout << regex << endl;    
+    }
+    return regex;
+    // conjuntos []
+    // intervalos [-]
+    // complementos [^]
+    // um ou mais +
+
+}
+int main(void) {
+    string re = "a|b";
+
+    string regex = treatRegex(re);
+
+    Graph G(regex.length()+1, regex);
     G.createNFA();
     G.printGraph();
 
-    std::string word = "ACGTA";
-    std::cout << "reconhece '" << word << "'?" << std::endl;
-    std::cout << reconhece(word, G) << std::endl;
-    
+    string word = "aaa";
+    cout << "reconhece '" << word << "'?" << endl;
+    cout << reconhece(word, G) << endl;
 
     return 0;
 } 
